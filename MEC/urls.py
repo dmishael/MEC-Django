a4898ac724d141afe2296175106d27730d2f1f35
daniel mishael <dmishael91@gmail.com>
2019-02-28 22:17:30 -0500

    heroku.

--- MEC/urls.py
+++ MEC/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include                 
 from rest_framework import routers        
 from MEC_app import views 
+from . import views
 
 router = routers.DefaultRouter()                    
 router.register('appointments', views.AppointmentView, 'appointment')
@@ -11,5 +12,7 @@ router.register('images', views.ImageView, 'image')
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    url(r'^$', views.FrontendAppView.as_view())
+
 ]
